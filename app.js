4c39c9964512d84b642962855eb9eac9f2484299
lizasilent <elizavetad@yandex.ru>
2021-04-12 23:54:25 +0300

    models and node rules

---
+++ app.js
@@ -0,0 +1,87 @@
+/* eslint-disable linebreak-style */
+require('dotenv').config();
+const express = require('express');
+
+const mongoose = require('mongoose');
+const { errors, celebrate, Joi } = require('celebrate');
+const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const { createUser, login } = require('./controllers/users');
+const NotFoundError = require('./errors/not-found-err');
+const auth = require('./middlewares/auth');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+// eslint-disable-next-line no-console
+}).then(() => console.log('Connected to DS'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  }
+  next();
+});
+app.use(express.json()); // для собирания JSON-формата
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required(),
+//   }),
+// }), login);
+
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     name: Joi.string().min(2).max(30),
+//     about: Joi.string().min(2).max(30),
+//     avatar: Joi.string().pattern(/^https?:\/\/(www\.)?([a-zA-Z0-9-])+\.([a-zA-Z])+\/?([a-zA-Z0-9\-_~:/#[\]@!&â€™,;=]+)/),
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required(),
+//   }),
+// }), createUser);
+
+app.use(auth);
+app.use('/', usersRouter);
+app.use('/', moviesRouter);
+app.use('/*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+app.use(errorLogger);
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? `На сервере произошла ошибка${err}`
+        : message,
+    });
+});
+
+app.listen(PORT, () => (
+  // eslint-disable-next-line no-console
+  console.log(PORT)
+));
---
+++ models/movie.js
@@ -0,0 +1,77 @@
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+      // eslint-disable-next-line no-useless-escape
+        return /^https?:\/\/(www\.)?([a-zA-Z0-9\-])+\.([a-zA-Z])+\/?([a-zA-Z0-9\-\._~:\/\?#\[\]@!\$&’\(\)\*\+,;=]+)/.test(v);
+      },
+      message: (props) => `Ошибка в ссылке ${props.value}`,
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+      // eslint-disable-next-line no-useless-escape
+        return /^https?:\/\/(www\.)?([a-zA-Z0-9\-])+\.([a-zA-Z])+\/?([a-zA-Z0-9\-\._~:\/\?#\[\]@!\$&’\(\)\*\+,;=]+)/.test(v);
+      },
+      message: (props) => `Ошибка в ссылке ${props.value}`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+      // eslint-disable-next-line no-useless-escape
+        return /^https?:\/\/(www\.)?([a-zA-Z0-9\-])+\.([a-zA-Z])+\/?([a-zA-Z0-9\-\._~:\/\?#\[\]@!\$&’\(\)\*\+,;=]+)/.test(v);
+      },
+      message: (props) => `Ошибка в ссылке ${props.value}`,
+    },
+  },
+  owner: {
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model('movie', movieSchema);
